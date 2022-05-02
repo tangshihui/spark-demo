4618bcf9035da76b69cc895d69cc66c5ffccc8e6
tangshihui <tsh_index@163.com>
2022-05-02 11:50:31 +0800

    add comment

--- src/main/scala/com/example/spark/BroadcastTest.scala
+++ src/main/scala/com/example/spark/BroadcastTest.scala
@@ -7,6 +7,12 @@ object BroadcastTest {
   /**
    *
    * broadcast variable
+   *  - 普通变量在每一个task上都会copy一份，网络IO开销大
+   *  - broadcast变量只需在executor上copy一份，executor上的tasks共享这份数据，减少不必要的网络IO
+   *
+   * accumulator
+   *  - 普通变量的累加只会在task上执行累加结果，在driver端获取不到累加结果
+   *  - accumulator变量可以将task累加的结果发送到driver端，driver端可以汇总所有task的累加结果
    *
    * @param args
    */
